c04acf6822c6ca08967262ddb16abe9895ede602
Unknown <saberchicken@gmail.com>
2017-10-08 01:40:03 -0700

    Revert "Fixed incorrect merge"
    This reverts commit 044c32c72603ac0a7a1dfdce0db0cfd8246d5e0d.

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,39 +1,20 @@
 function fizzBuzz(start, end){
-2
     // base case
-3
     if(start == 0){
-4
         console.log(end-start);
-5
         return 0;
-6
     }
-7
-8
     // recursive case
-9
     if((end - start) % 15 == 0){
-10
         console.log("Fizzbuzz");
-11
     } else if((end - start) % 5 == 0){
-12
         console.log("Buzz");
-13
     } else if((end - start) % 3 == 0){
-14
         console.log("Fizz");
-15
     } else {
-16
         console.log(end-start);
-17
     }
-18
     fizzBuzz(start-1,end);
-19
 }
-20
-21
+
 fizzBuzz(99,100);
