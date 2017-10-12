08c8d4a2fa10bed8621db3f00cb033141f45731c
Johnny Reina <johnny.r.reina@gmail.com>
2017-10-12 18:55:48 -0500

    Gets message or number from pairs.

--- haskell/fizzBuzz.hs
+++ haskell/fizzBuzz.hs
@@ -1,4 +1,6 @@
 isA num message val = if mod val num == 0 then message else ""
 fizzBuzz x = isA 3 "Fizz" x ++ isA 5 "Buzz" x
-fizzBuzzes xs = [ (x, fizzBuzz x) | x <- xs]
+pairs xs = [ (x, fizzBuzz x) | x <- xs]
+justMessage (a, b) = if length b == 0 then show a else b
+fizzBuzzes xs = [ justMessage x | x <- pairs xs ]
 main = print $ fizzBuzzes [1..100]
