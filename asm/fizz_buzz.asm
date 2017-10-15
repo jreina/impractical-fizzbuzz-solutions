33b94afc8720d4121d99d3ef18d837d104375ac2
Zumza123 <coctavian10@yahoo.ro>
2017-10-15 17:17:25 +0300

    Update fizz_buzz.asm

--- asm/fizz_buzz.asm
+++ asm/fizz_buzz.asm
@@ -1,12 +1,12 @@
 section .data
-  fizzbuzz_msg db "FizzBuzz", 0xA
-  fizzbuzz_len equ $ - fizzbuzz_msg
 
-  fizz_msg db "Fizz", 0xA
+  fizz_msg db "Fizz"
   fizz_len equ $ - fizz_msg
 
-  buzz_msg db "Buzz", 0xA
+  buzz_msg db "Buzz"
   buzz_len equ $ - buzz_msg
+
+  new_line db 0xA
 
 section .text
   global _start
@@ -14,61 +14,65 @@ section .text
   _start:
     mov ebp, esp
     mov dword [ebp - 4], 1
+    mov byte [ebp - 5], 0
 
-  check_fizzbuzz:
-    mov eax, [ebp - 4]
-    mov ebx, 15
-    cdq
-    idiv ebx
+    check_fizz:
+      mov eax, [ebp - 4]
+      mov ebx, 3
+      cdq
+      div ebx
 
-    test edx, edx
-    jne check_fizz
+      test edx, edx
+      jne check_buzz
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, fizzbuzz_msg
-    mov edx, fizzbuzz_len
-    int 0x80
+      mov byte [ebp - 5], 1
 
-  check_fizz:
-    mov eax, [ebp - 4]
-    mov ebx, 3
-    cdq
-    div ebx
+      mov eax, 4
+      mov ebx, 1
+      mov ecx, fizz_msg
+      mov edx, fizz_len
+      int 0x80
 
-    test edx, edx
-    jne check_buzz
+    check_buzz:
+      mov eax, [ebp - 4]
+      mov ebx, 5
+      cdq
+      div ebx
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, fizz_msg
-    mov edx, fizz_len
-    int 0x80
+      test edx, edx
+      jne check_num
 
-  check_buzz:
-    mov eax, [ebp - 4]
-    mov ebx, 5
-    cdq
-    div ebx
+      mov byte [ebp - 5], 1
 
-    test edx, edx
-    jne continue
+      mov eax, 4
+      mov ebx, 1
+      mov ecx, buzz_msg
+      mov edx, buzz_len
+      int 0x80
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buzz_msg
-    mov edx, buzz_len
-    int 0x80
+    check_num:
+      cmp byte [ebp - 5], 1
+      jne continue
 
-  continue:
-    mov eax, [ebp - 4]
-    add eax, 1
-    cmp eax, 101
-    je exit
-    mov dword [ebp - 4], eax
-    jmp check_fizzbuzz
+    continue:
+      mov eax, [ebp - 4]
+      add eax, 1
 
-  exit:
-    mov eax, 1
-    mov ebx, 0
-    int 0x80
+      cmp eax, 101
+      je exit
+
+      mov dword [ebp - 4], eax
+      mov byte [ebp - 5], 0
+
+      mov eax, 4
+      mov ebx, 1
+      mov ecx, new_line
+      mov edx, 0x2
+      int 0x80
+
+      jmp check_fizz
+
+    exit:
+      mov eax, 1
+      mov ebx, 0
+      int 0x80
